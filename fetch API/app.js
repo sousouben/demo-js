8c6d846b5d513378d43e86b147e532254e62bbf7
sousou ben <s.mouatakide@laposte.net>
2021-04-07 11:39:00 +0200

    ajout dossier challenge météo

--- fetch API/app.js
+++ fetch API/app.js
@@ -2,7 +2,19 @@ document.querySelector('#cp').addEventListener('input', function () {
     if (this.value.length == 5) {//si la longueur est = 5 caractères
         let url = `https://geo.api.gouv.fr/communes?codePostal=${this.value}`;
 
-        fetch(url).then(Response => Response.json().then(data => console.log(data))
-        );
+        fetch(url)
+        .then((Response) =>//formater la reponce
+            Response.json()
+            .then((data) => {//traiter les données
+                //console.log(data); pour avoir la liste des tableaux
+                let affichage = '<ul>';
+                for (let ville of data) {
+                    affichage += `<li><strong>${ville.nom}</strong> - ${ville.population} habitants</li>`;
+                }
+                affichage += '</ul>';
+                document.querySelector('#villes').innerHTML = affichage;
+            })
+        )
+        .catch((err) => console.log('erreur:'+ err));// au cas ou l'appel à l'url ne fonctionne pas
     }
 });
